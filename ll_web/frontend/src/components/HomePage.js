b045fe256463f8d0a2bf17dbe5c800876ee262fc
Kyattsukuro <seccondacc01@gmail.com>
2023-04-27 09:04:15 +0200

    / view edited and setup propper .gitignore

--- ll_web/frontend/src/components/HomePage.js
+++ ll_web/frontend/src/components/HomePage.js
@@ -8,6 +8,14 @@ export default class HomePage extends Component {
     }
 
     render(){
-        return <p>Home</p>
+        return <div>
+        <div class="top_bar">
+            <p>Username</p>
+            <p>{ Date() }</p>
+            <button>Settings</button>
+        </div>
+        <p1 id="headline">Link Liste</p1>
+        <div class="main_contend">Contend div link liste</div>
+    </div>
     }
 }
