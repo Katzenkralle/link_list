85cd1b85bef884de3648af1fc0fa6d794edb1062
someone <seccondacc01@gmail.com>
2023-05-12 23:44:45 +0200

    Aded Tag creation frontend window

--- ll_web/frontend/src/components/App.js
+++ ll_web/frontend/src/components/App.js
@@ -1,22 +1,20 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
+import React from "react";
+import { createRoot } from "react-dom/client";
 
 import HomePage from './HomePage';
 import { BrowserRouter as Router,  Route, Routes} from "react-router-dom";
 import ViewList from "./ViewList";
 
 function App() {
-    
-        return (
-            <Router>
-                <Routes>
-                    <Route exact path="/" element= {<HomePage />} />
-                    <Route path="view" element= {<ViewList />} />
-                </Routes>
-          </Router>
-        );
-    
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<HomePage />} />
+                <Route path="view" element={<ViewList />} />
+            </Routes>
+        </Router>
+    );
 }
 
 const appDiv = document.getElementById('app');
-render(<App />, appDiv);
+createRoot(appDiv).render(<App />);
